6a97cf4c903762275afc802691a596e69b8db381
HojinPark <qkrghwls101@gmail.com>
2025-05-27 15:41:05 +0000

    fix: modified set_dataset_symlink.sh to align with training code

--- bash/set_dataset_symlink.sh
+++ bash/set_dataset_symlink.sh
@@ -1,3 +1,3 @@
-mkdir dataset
-ln -s /workspace/open/train dataset/train
-ln -s /workspace/open/test dataset/test
+mkdir /workspace/dataset
+ln -s /workspace/open/train /workspace/dataset/train
+ln -s /workspace/open/test /workspace/dataset/test
